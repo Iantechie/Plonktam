b5de331b24cf902647ff8284a106b1531e74d1e8
Iantechie <kipronoian68@gmail.com>
2023-09-28 23:50:38 +0300

    ..

--- versioning.py
+++ versioning.py
@@ -1,7 +1,7 @@
 import sys
 from packaging.version import Version
 
-current_tag = sys.argv[1] if len(sys.argv) else '0.0.1'
+current_tag = sys.argv[1] if len(sys.argv) > 1 else '0.0.1'
 version = Version(current_tag)
 new_version = Version(f"{version.major}.{version.minor + 1}")
 print(new_version)
