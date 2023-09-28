8753dd2bc57f04e07f786431a18935f507e203a1
Iantechie <kipronoian68@gmail.com>
2023-09-28 15:14:27 +0300

    feat(ui): add dynamic data

--- app1/views.py
+++ app1/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
+from .models import CoverDetails
 # Create your views here.
 def index(request):
-    return render(request, 'app1/index.html')
+    list_data = CoverDetails.objects.all()
+    context = {
+        'list_data':list_data
+    }
+    return render(request, 'app1/index.html', context)
