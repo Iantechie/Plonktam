9aea4679d037fe39097de21fc5ae1e92ac287c50
Iantechie <kipronoian68@gmail.com>
2023-10-20 19:29:10 +0300

    patch(user): add custom user model

--- app1/views.py
+++ app1/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import CoverDetails
+
 # Create your views here.
 def index(request):
     list_data = CoverDetails.objects.all()
@@ -7,3 +8,11 @@ def index(request):
         'list_data':list_data
     }
     return render(request, 'app1/index.html', context)
+
+def cover_details(request, pk):
+    cover = get_object_or_404(CoverDetails, id=pk)
+    
+    context = {
+        'details': cover
+    }
+    return render(request, 'app1/cover_detail.html', context)
